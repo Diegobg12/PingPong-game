595ba6941ceb2be82192a6711f69d93740da5dc3
Diego Bustos <diegobustos1229@gmail.com>
2019-11-06 20:28:09 -0800

    Add movement to the ball

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -1,21 +1,41 @@
 import { SVG_NS, BOARD_COLOR, PADDLE_COLOR } from '../settings';
 
 export default class Ball {
-    constructor(radio, x, y) {
-
-      this.radio = radio;
-      this.x = x;
-      this.y = y;
+    constructor(radio, boardWidth, boardHeight) {
+        this.radio = radio;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        this.direction = 1;
+        this.x = this.boardWidth/2;
+        this.y = this.boardHeight/2;
+        this.reset();
     }
+
+    ballMove(){
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+    }
+
+    reset(){
+        this.x = this.boardWidth/2;
+        this.y = this.boardHeight/2;
+        this.vy = 0;
+        while (this.vy === 0){
+            this.vy =   Math.floor(Math.random()*10)-5;
+        }
+        this.vx = this.direction * (6-Math.abs(this.vy));
+
+    }
+
     render(svg) {
       //Create Ball
     //   <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="red" />
-          let board = document.createElementNS(SVG_NS, "circle");
-          board.setAttributeNS(null, "fill", PADDLE_COLOR);
-          board.setAttributeNS(null, "r", this.radio);
-          board.setAttributeNS(null, "cx", this.x);
-          board.setAttributeNS(null, "cy", this.y);
-          svg.appendChild(board);
- 
+          let ball = document.createElementNS(SVG_NS, "circle");
+          ball.setAttributeNS(null, "fill", PADDLE_COLOR);
+          ball.setAttributeNS(null, "r", this.radio);
+          ball.setAttributeNS(null, "cx", this.x);
+          ball.setAttributeNS(null, "cy", this.y);
+          svg.appendChild(ball);
+        this.ballMove();
     }
   }
